805267bceae67e3042673ac6df17151dcfac84e9
Colby Todd <cjhtodd11@gmail.com>
2024-10-29 15:01:41 -0400

    Added stuff

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Author} from '../model/author';
+import {AuthorsService} from '../service/authors.service';
+import {Subscription} from "rxjs";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-authors',
@@ -8,5 +13,26 @@ import { Component } from '@angular/core';
   styleUrl: './authors.component.css'
 })
 export class AuthorsComponent {
+  selectedAuthor!: Author | null;
+  private subscription!: Subscription;
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private authorsService: AuthorsService = inject(AuthorsService);
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.subscription = this.authorsService.getBook(id).subscribe({
+        next: (data: Author) => {
+          this.selectedAuthor = data;
+        },
+        error: (_: any) => {
+          this.selectedAuthor = null;
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
